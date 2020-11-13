597a1a110c5d65672541286d830f7a6ee8f2b76b
winningarc <shokim01@gmail.com>
2020-11-13 20:07:52 +0900

    fix: async authenticate

--- index.js
+++ index.js
@@ -8,24 +8,28 @@ const models = require("./database/models/index.js");
 const helmet = require("koa-helmet");
 // const { jwtMiddleware } = require("./src/utils");
 
-const app = new Koa();
+const run = async () => {
+  const app = new Koa();
 
-try {
-  await models.sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
+  try {
+    await models.sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+  app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
+  app.use(helmet());
+  app.use(logger());
+  app.use(bodyParser());
+  // app.use(jwtMiddleware);
+  app.use(router.routes()).use(router.allowedMethods());
+
+  const PORT = 8080;
+
+  app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+  });
 }
 
-app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
-app.use(helmet());
-app.use(logger());
-app.use(bodyParser());
-// app.use(jwtMiddleware);
-app.use(router.routes()).use(router.allowedMethods());
-
-const PORT = 8080;
-
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
-});
+run()
