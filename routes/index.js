fd79cdc9ffde3c48f10f6793238eee0c8555d589
winningarc <shokim01@gmail.com>
2020-11-13 17:19:27 +0900

    feat: add test register

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const Router = require("koa-router");
 
+const user = require("./user");
+
 const router = new Router();
+
+router.use("/register", user.routes());
 
 module.exports = router;
---
+++ routes/user/index.js
@@ -0,0 +1,7 @@
+const Router = require("koa-router");
+const user = new Router();
+const userCtrl = require("./user.ctrl");
+
+user.post("/", userCtrl.register);
+
+module.exports = user;
