3de3e7259f28b432cfaacf465c6ecca568db834e
Quasar0311 <johnking0311@gmail.com>
2020-11-19 23:59:01 +0900

    answer post & list & clap

--- routes/answer/answer.js
+++ routes/answer/answer.js
@@ -1,14 +1,48 @@
 const models = require('@models');
 
 exports.answering = async (ctx) => {
-  console.log('received');
-  const { question, user, content,clap } = ctx.request.body;
+  console.log('answering');
+  const { question, user, content } = ctx.request.body;
   const new_answer = await models.Answer.create({
     question, 
     user, 
     content,
-    clap
+    clap: 0,
   });
   ctx.assert(new_answer, 500);
   ctx.status = 204;
 };
+
+exports.answerlist = async (ctx) => {
+  console.log('answering/list');
+  const { question } = ctx.request.body;
+
+  const answers = await models.Answer.findAll({
+    where: { question: question }
+  });
+  
+  ctx.body = answers;
+  ctx.status = 200;
+};
+
+exports.clap = async (ctx) => {
+  console.log('answering/clap');
+  const { id, user } = ctx.request.body;
+  
+  const answer = await models.Answer.findOne({
+    where: { id: id }
+  });
+
+  ctx.assert(answer, 401);
+
+  if (answer.user == user) {
+    ctx.body = answer.clap;
+  }
+  else {
+    answer.update({ clap: answer.clap + 1 });
+    ctx.body = answer.clap;
+  }
+  console.log(answer);
+  ctx.status = 200;
+
+}
--- routes/answer/index.js
+++ routes/answer/index.js
@@ -5,5 +5,7 @@ const answerRouter = new Router();
 const answer = require("./answer")
 
 answerRouter.post("/", answer.answering)
+answerRouter.post("/list", answer.answerlist)
+answerRouter.post("/clap", answer.clap)
 
 module.exports = answerRouter;
