a44ba104571e0e53b44f6b99d60b941d6f676a18
winningarc <shokim01@gmail.com>
2020-11-20 17:13:25 +0900

    fix index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 require('module-alias/register');
 const Koa = require('koa');
+const helmet = require('koa-helmet');
 const bodyParser = require('koa-body');
 const cors = require('@koa/cors');
 const logger = require('koa-logger');
-const router = require('./routes');
 const models = require('@models');
-const helmet = require('koa-helmet');
+const router = require('./routes');
 const { jwtMiddleware } = require('./utils/jwt');
 
 const PORT = 8080;
