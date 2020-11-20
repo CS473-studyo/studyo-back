823c270e498935e50f4f56feca5c5263234d258c
Sunjuhyeong <67995959+Sunjuhyeong@users.noreply.github.com>
2020-11-20 21:05:31 +0900

    Debug

--- routes/user/user.ctrl.js
+++ routes/user/user.ctrl.js
@@ -18,8 +18,6 @@ exports.register = async (ctx) => {
 };
 
 exports.login = async (ctx) => {
-  ctx.assert(ctx.request.user, 401);
-  const userId = ctx.request.user.id;
   const { email, password } = ctx.request.body;
   const res = await models.User.findOne({ where: { email } });
   ctx.assert(res, 204);
