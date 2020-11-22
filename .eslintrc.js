90b8ab153e194338d0fe76c5ede8a6efd221dc48
winningarc <shokim01@gmail.com>
2020-11-22 15:53:40 +0900

    add rule to esling

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'jsx-a11y/href-no-hash': ['off'],
     'import/no-unresolved': ['off'],
     'func-names': ['off'],
+    'object-shorthand': ['off'],
     'max-len': [
       'warn',
       {
