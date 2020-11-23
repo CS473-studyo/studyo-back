f723bfe7a89412f9fdacc804bd395cfd9e6454ac
winningarc <shokim01@gmail.com>
2020-11-24 06:11:50 +0900

    fix api link

--- routes/lecture/index.js
+++ routes/lecture/index.js
@@ -6,6 +6,6 @@ const lecture = require('./lecture');
 
 lectureRouter.get('/:CourseId', lecture.courseLectures);
 lectureRouter.post('/pdf/:LectureId', lecture.uploadPdf);
-lectureRouter.get('/info/:LectureId', lecture.lectureInfo);
+lectureRouter.get('/:LectureId', lecture.lectureInfo);
 
 module.exports = lectureRouter;
